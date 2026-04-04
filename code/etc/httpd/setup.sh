65e89f8748670acfeb8085b27b3f115d493e2697
Takamatsu Makoto <tkmtmkt@gmail.com>
2026-04-05 05:01:22 +0900

    fix: tls証明書のsubjのホスト名修正

--- code/etc/httpd/setup.sh
+++ code/etc/httpd/setup.sh
@@ -20,7 +20,7 @@ ln -sf {${SCRIPT_DIR},/etc/httpd}/conf.modules.d/50-code.conf
 echo -e "### ssl設定用の自己証明書作成"
 openssl req -out /etc/pki/tls/certs/localhost.crt \
   -newkey ED25519 -noenc -keyout /etc/pki/tls/private/localhost.key \
-  -x509 -subj "/C=JP/ST=Toaru-ken/L=Toaru-shi/O=X/OU=X/CN=example.com" -days 365
+  -x509 -subj "/C=JP/ST=Toaru-ken/L=Toaru-shi/O=X/OU=X/CN=localhost" -days 365
 
 echo -e "### httpdサービス有効化"
 systemctl enable httpd.service
